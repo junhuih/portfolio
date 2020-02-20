7c9d0382822be48879dedd1bd494c62c1f052e5d
Mark He <junhuih@andrew.cmu.edu>
2020-02-19 21:57:59 -0500

    everything can move left

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,19 @@ $(document).ready(function () {
 	$('.toshow h1').delay(800).animate({'opacity':'1'},1200);
 	$('.toshow p').delay(1700).animate({'opacity':'1'},1200);
 });
+
+function myMove() {
+  var elems = document.querySelectorAll(".gallery");
+  
+  var width = elems[0].offsetWidth
+  var mov = 0;
+  setInterval(frame, 2);
+  function frame() {
+    if (mov < width) {
+      for (i = 0; i < elems.length; i++){
+        elems[i].style.left = -mov + 'px'; 
+      }
+      mov++;
+    }
+  }
+}
