e315e997f713168c67200246c4b27da90e39b4e5
Mark He <junhuih@andrew.cmu.edu>
2021-05-31 22:54:13 +0800

    edit like button

--- qihengheng/js/javascript.js
+++ qihengheng/js/javascript.js
@@ -1,4 +1,6 @@
 function toggleLike() {
     var like = document.getElementById("like");
     like.src = "../images/like-after.jpeg";
+    var like_count = document.getElementById("like_count");
+    like_count.textContent = (parseInt(like_count.innerHTML) + 1);
 }
